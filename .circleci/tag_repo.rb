5982e281f785328742a7b10ff906e6d1d885c632
Jocko <jocko.macgregor@wowza.com>
2019-04-02 18:09:48 -0600

    Hopefully this is clear enough.

--- .circleci/tag_repo.rb
+++ .circleci/tag_repo.rb
@@ -11,19 +11,21 @@ def usage
 	puts "  integration          Run integration tests upon commit"
   puts "  unit                 Run unit tests upon commit"
   puts "  release              Release the Gem asset (includes the unit and integration tags)"
-  puts "  publish              Publish the code documentation"
+  puts "  publish-staging      Publish the code documentation to the staging docs site."
+  puts "  publish-production   Publish the code documentation to the production docs site."
   puts ""
   exit
 end
 
-available_tags = {
-	"integration"  => "run-integration-tests",
-	"unit"         => "run-unit-tests",
-	"release"      => "release",
-	"publish"      => "publish"
-}
+available_tags = [
+	"integration",
+	"unit",
+	"release",
+	"publish-staging",
+  "publish-production"
+]
 
-tags      = (available_tags.keys & ARGV).map{ |t| t.downcase }.flatten
+tags      = (available_tags & ARGV).map{ |t| t.downcase }.flatten
 
 # Generate usage if we got a bad tag.
 usage if (ARGV - tags).length > 0
@@ -34,29 +36,11 @@ tags = tags - ["publish"] if tags.include?("release")
 
 puts "Tags: #{tags}"
 
-tags.each do |tag_key|
-  tag     = available_tags[tag_key]
+tags.each do |tag|
   puts "Tagging the repo with #{tag}..."
-	# puts "."
   `cd #{WORK_DIR} && git push origin ":#{tag}" > /dev/null 2>&1`
   `cd #{WORK_DIR} && git tag --delete #{tag} > /dev/null 2>&1`
   `cd #{WORK_DIR} && git tag -a #{tag} -m "Used tag-for-build.sh on component=${component}" > /dev/null 2>&1`
 	`cd #{WORK_DIR} && git push --tags > /dev/null 2>&1`
 	puts " |> Tag updated!"
 end
-#
-# 	tag="start-component-build/${component}"
-# 	puts
-# 	puts "Tagging the repo with #{tag} for component=${component}..."
-# 	# puts "."
-# 	git push origin ":#{tag}" > /dev/null 2>&1
-# 	# puts "."
-# 	git tag --delete #{tag} > /dev/null 2>&1
-# 	# puts "."
-# 	git tag -a #{tag} -m "Used tag-for-build.sh on component=${component}" > /dev/null 2>&1
-# 	# puts "."
-# 	git push --tags > /dev/null 2>&1
-# 	puts "Done Updating Tags"
-# 	puts
-#
-# done
